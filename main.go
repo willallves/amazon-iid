c5c8247b2806e142b11bdb0d3e4797d3310c7469
Willian Alves <wiilliian.alves@gmail.com>
2022-07-26 13:54:36 -0400

    bug pkcs7: to verify error: algorithm unimplemented

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/willallves/amazon-iid/pkcs7"
 	"github.com/willallves/amazon-iid/rsa"
 )
 
 func main() {
 	rsa.VerifyRSACert()
+
+	pkcs7.VerifyPKCS7Cert()
 }
